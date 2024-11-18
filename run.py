fff769681159943685f938c4a6c9e0daa9ac12fa
EvgenTretyak <120394423+EvgenTretyak@users.noreply.github.com>
2024-11-18 12:45:26 +0300

    fix1

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from aiogram import F
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from aiogram.methods import DeleteWebhook
